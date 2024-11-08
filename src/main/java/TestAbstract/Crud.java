675f63abaeb51f52a731e8100f686e76795e76d5
ADMIN <ADMIN@Ly>
2024-11-08 09:32:07 +0700

    Add TestDAO

---
+++ src/main/java/TestAbstract/Crud.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TestAbstract;
+
+/**
+ *
+ * @author ADMIN
+ */
+public abstract class Crud {
+    public abstract void create();
+    public abstract void reset();
+    public abstract void update();
+    public abstract void delete();
+}
